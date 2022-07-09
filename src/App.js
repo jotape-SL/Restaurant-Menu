80f454ab882e5967aeca33f9fab31e7fe325baf5
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-07-09 11:58:43 -0300

    Buttons and funcionality

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    setMenuItems(items.filter((item) => item.category === category));
+  };
+
   return (
     <main>
       <section className='menu section'>
@@ -13,7 +24,7 @@ function App() {
           <h2>Our Menu</h2>
           <div className='underline'></div>
         </div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
